856182f3e9cf47b57c3f12f12563d791f87fb8b6
xlakostax <konstantin.veselovskii@gmail.com>
2023-05-01 12:56:16 +0300

    Chatgpt API communication logic both for voice and text messages has been added

--- src/main.js
+++ src/main.js
@@ -1,37 +1,72 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, session } from "telegraf";
+import { code } from "telegraf/format";
 import { message } from "telegraf/filters";
 import { ogg } from "./ogg.js";
 import { openai } from "./openAi.js";
 import config from "config";
 
+const INITIAL_SESSION = {
+  messages: [],
+}
+
 const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
 
+bot.use(session());
+
+bot.command("new", async (context) => {
+  context.session = INITIAL_SESSION;
+  await context.reply("Awaiting of your text or voice message...")
+})
+
+bot.command("start", async (context) => {
+  context.session = INITIAL_SESSION;
+  await context.reply("Awaiting of your text or voice message...")
+})
+
 bot.on(message("voice"), async (context) => {
+  context.session ??= INITIAL_SESSION;
   try {
+    await context.reply(code("Message accepted. Please, await a response from the server..."))
     const link = await context.telegram.getFileLink(context.message.voice.file_id);
     const userId = String(context.message.from.id);
     const oggPath = await ogg.create(link.href, userId);
     const mp3Path = await ogg.toMp3(oggPath, userId);
     const text = await openai.transcription(mp3Path);
-    const response = await openai.chat(text);  
-    await context.reply(JSON.stringify(link, null, 2));
-    await context.reply(mp3Path);
+    await context.reply(code(`Your response: ${text}`));
+    context.session.messages.push({ 
+      content: text, 
+      role: openai.roles.USER 
+    });
+    const response = await openai.chat(context.session.messages);
+    context.session.messages.push({ 
+      content: text, 
+      role: openai.roles.ASSISTANT 
+    });
+    await context.reply(response.content);
   } catch (error) {
-    console.log(`Error while running: ${error.message}`); 
+    console.log(`Error while processing voice message: ${error.message}`); 
   }
 });
 
-bot.on(message("text"), async (context) => {
+bot.on(message('text'), async (context) => {
+  context.session ??= INITIAL_SESSION;
   try {
-    await context.reply(JSON.stringify(context.message.text, null, 2))
+    await context.reply(code(`Your message "... ${context.message.text} ..." has been accepted. Please, await a response from the server...`))
+    context.session.messages.push({ 
+      role: openai.roles.USER,
+      content: context.message.text, 
+    });
+    const response = await openai.chat(context.session.messages);
+    context.session.messages.push({ 
+      role: openai.roles.ASSISTANT,
+      content: response.content, 
+    });
+    await context.reply(response.content);
   } catch (error) {
-    console.log(`Error while running: ${error.message}`)    
+    console.log(`Error while processing text message: ${error.message}`); 
   }
-});
-
-bot.command("start", async (context) => {
-  await context.reply(JSON.stringify('', null, 2))
 })
+
 bot.launch();
 
 process.once("SIGINT", () => bot.stop("SIGINT"))
--- src/openAi.js
+++ src/openAi.js
@@ -1,9 +1,44 @@
+import { Configuration, OpenAIApi } from "openai";
+import config from "config";
+import { createReadStream } from "fs";
+
 class OpenAI {
-  constructor() {}
+  roles = {
+    ASSISTANT: 'assistant',
+    USER: 'user',
+    SYSTEM: 'system',
+  }
 
-  chat() {};
+  constructor(apiKey) {
+    const configuration = new Configuration({
+      apiKey,
+    })
+    this.openai = new OpenAIApi(configuration)
+  }
 
-  transcription() {};
+  async chat(messages) {
+    try {
+      const response = await this.openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages,
+      })
+      return response.data.choices[0].message
+    } catch (error) {
+      console.log('Error while getting message: ', error.message)
+    }
+  }
+
+  async transcription(filepath) {
+    try {
+      const response = await this.openai.createTranscription(
+        createReadStream(filepath),
+        'whisper-1'
+      )
+      return response.data.text
+    } catch (error) {
+      console.log('Error while file transcription: ', error.message)
+    }
+  }
 }
 
-export const openai = new OpenAI();
+export const openai = new OpenAI(config.get("OPENAI_KEY"));
